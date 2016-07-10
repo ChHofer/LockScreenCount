3519d2ad59667082d5e22cc2eab6d9154c6108fd
ChHofer <chofer@edu.aau.at>
2016-07-10 17:15:46 +0200

    Version 1.0

--- app/src/main/java/com/flashtea/lockscreencount/MainActivity.java
+++ app/src/main/java/com/flashtea/lockscreencount/MainActivity.java
@@ -1,13 +1,143 @@
 package com.flashtea.lockscreencount;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int NOTIFICATION_ID = 999;
+    private NotificationManager notificationManager;
+    private NotificationCompat.Builder mNotifyBuilder;
+    RemoteViews notificationView;
+    int count = 0;
+    Button showNotificationButton;
+    Button removeNotificationButton;
+
+    BroadcastReceiver broadcastReceiverAdd = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            count++;
+            updateNotification();
+        }
+    };
+
+    BroadcastReceiver broadcastReceiverReset = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            count=0;
+            updateNotification();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerReceiver(broadcastReceiverAdd, new IntentFilter("ADD_COUNT"));
+        registerReceiver(broadcastReceiverReset, new IntentFilter("RESET_COUNT"));
+
+        showNotificationButton = (Button) findViewById(R.id.show_notification_button);
+        showNotificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNotification();
+            }
+        });
+
+        removeNotificationButton = (Button) findViewById(R.id.remove_notification_button);
+        removeNotificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeNotification();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiverAdd);
+        unregisterReceiver(broadcastReceiverReset);
+    }
+
+    private void startNotification(){
+        String ns = Context.NOTIFICATION_SERVICE;
+        notificationManager = (NotificationManager) getSystemService(ns);
+
+
+        notificationView = new RemoteViews(getPackageName(),
+                R.layout.activity_custom_notification);
+
+        //the intent that is started when the notification is clicked (works)
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingNotificationIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                .setContent(notificationView)
+                .setContentIntent(pendingNotificationIntent)
+                .setContentTitle("New Message")
+                .setContentText("You've received new messages.")
+                .setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+
+        //Intent that is started when Add Button is clicked.
+        Intent addIntent = new Intent(this, addButtonListener.class);
+        PendingIntent pendingAddIntent = PendingIntent.getBroadcast(this, 0,
+                addIntent, 0);
+
+        notificationView.setOnClickPendingIntent(R.id.add,
+                pendingAddIntent);
+
+
+        //Intent that is started when Reset Button is clicked.
+        Intent resetIntent = new Intent(this, resetButtonListener.class);
+        PendingIntent pendingResetIntent = PendingIntent.getBroadcast(this, 0,
+                resetIntent, 0);
+
+        notificationView.setOnClickPendingIntent(R.id.reset,
+                pendingResetIntent);
+
+        notificationManager.notify(NOTIFICATION_ID, mNotifyBuilder.build());
+    }
+
+    public void removeNotification(){
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    public static class addButtonListener extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            context.sendBroadcast(new Intent("ADD_COUNT"));
+        }
+    }
+
+    public static class resetButtonListener extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            context.sendBroadcast(new Intent("RESET_COUNT"));
+        }
+    }
+
+    public void updateNotification(){
+        notificationView.setTextViewText(R.id.appName,String.valueOf(count));
+        mNotifyBuilder.setContent(notificationView);
+
+        // Because the ID remains unchanged, the existing notification is
+        // updated.
+        notificationManager.notify(NOTIFICATION_ID, mNotifyBuilder.build());
     }
 }
